4580d715fa37693ba0c3448cdb2961e23ab56eaf
Debbie <debiurquhart@live.co.uk>
2020-01-27 16:45:31 +0000

    init

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,16 @@
+public class Calculator {
+
+    public Calculator(){
+
+    }
+
+    public int add(int number1, int number2) {
+        return (number1 + number2);
+    }
+}
+
+
+//Calculator
+//        Create a Calculator class. This should have functions for Add, Subtract, Multiply and Divide.
+//        Your methods should take in two ints to perform the calculations on (except the Divide method.
+//        This should take two doubles as arguments).
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,20 @@
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    private Calculator calculator;
+
+    @Before
+    public void before(){
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void canAdd(){
+        assertEquals(4, calculator.add(2, 2));
+    }
+}
