3cd911dac4f08c2644a6292c24692890db1e7676
Debbie <debiurquhart@live.co.uk>
2020-01-27 16:53:04 +0000

    adds calculator class

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -17,4 +17,19 @@ public class CalculatorTest {
     public void canAdd(){
         assertEquals(4, calculator.add(2, 2));
     }
+
+    @Test
+    public void canSubtract() {
+        assertEquals( 3, calculator.subtract(7, 4));
+    }
+
+    @Test
+    public void canMultiply() {
+        assertEquals(25, calculator.multiply(5, 5));
+    }
+
+    @Test
+    public  void canDivide() {
+        assertEquals(4.0, calculator.divide(12.0, 3.0), 0.0001);
+    }
 }
