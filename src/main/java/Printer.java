447df2a1f3f130ba2c28e25c018d02c127923db2
Debbie <debiurquhart@live.co.uk>
2020-01-27 18:33:57 +0000

    adds printer test

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,2 +1,20 @@
 public class Printer {
+    private int paper;
+
+
+    public Printer(int paper) {
+        this.paper = paper;
+    }
+
+    public int paper() {
+        return this.paper();
+    }
 }
+
+
+//Printer
+//        Create a Printer class that has a property for number of sheets of paper left.
+//        Add a method to print that takes in a number of pages and number of copies.
+//        The print method will only run if the printer has enough paper. If it runs it will reduce the value of the paper left by number of copies * number of pages.
+//        Add a toner volume property to the class.
+//        Modify the printer so that it reduces the toner by 1 for each page printed.
--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -1,2 +1,41 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PrinterTest {
+
+    private  Printer printer;
+    @Before
+    public void before(){
+        printer = new Printer(500);
+    }
+
+    @Test
+    public void hasPaper(){
+        assertEquals(500, printer.paper());
+    }
+
+    @Test
+    public void canPrint(){
+        assertEquals(true, printer.print(14, 10));
+    }
+
+    @Test
+    public void hasToner(){
+        assertEquals(300, printer.toner());
+    }
+
+    @Test
+    public void reduceToner(){
+        assertEquals(160, printer.toner());
+    }
 }
+
+
+//Printer
+//        •	Create a Printer class that has a property for number of sheets of paper left.
+//        •	Add a method to print that takes in a number of pages and number of copies.
+//        •	The print method will only run if the printer has enough paper. If it runs it will reduce the value of the paper left by number of copies * number of pages.
+//        •	Add a toner volume property to the class.
+//        •	Modify the printer so that it reduces the toner by 1 for each page printed.
